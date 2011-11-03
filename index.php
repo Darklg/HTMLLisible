980ed9fdba0b462671febfe0bab00671b81e4098
Darklg <kev.rocher@gmail.com>
2011-11-03 13:59:51 +0100

    Ajout de CSS basique + Suppression de la todolist

--- index.php
+++ index.php
@@ -1,31 +1,25 @@
 <?php
-
-/*
-@TODO : Gestion des blocs PHP <?php ?>
-@TODO : Gestion des STYLE et SCRIPT
-@TODO : Gestion des balises inline en front
-@TODO : Style basique pour la page
-@TODO : Algo : Si une balise inline a un enfant block : reindenter
-*/
-
 include dirname(__FILE__).'/inc/header.php';
 ?>
 <!DOCTYPE HTML>
 <html lang="fr-FR">
 	<head>
-		<title>HTMLLisible</title>
 		<meta charset="UTF-8" />
+		<title>HTMLLisible</title>
+		<link rel="stylesheet" href="style.css" type="text/css" />
 	</head>
 	<body>
+		<div id="main-container">
 		<h1>HTMLLisible</h1>
-		<form action="" method="post">
+		<form class="cssn_form" action="" method="post">
 			<div>
-				<label for="html_to_clean">
-					HTML à nettoyer
-				</label>
+				<label for="html_to_clean">HTML à réindenter</label>
 				<textarea rows="30" cols="100" style="display:block;" name="html_to_clean" id="html_to_clean"><?php echo htmlentities($content, ENT_COMPAT,'utf-8'); ?></textarea>
 			</div>
-			<button>Reindent</button>
+			<div class="submit">
+				<button class="le-button"><span>Réindenter</span></button>				
+			</div>
 		</form>
+		</div>
 	</body>
 </html>
