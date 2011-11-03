c8e927283d9e47962466f2ecc94e895f4b6e5dff
Darklg <kev.rocher@gmail.com>
2011-11-03 15:35:23 +0100

    Reindentation index.php & Suppression code residuel

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 include dirname(__FILE__).'/inc/header.php';
-
-
-#echo $content;die;
-
-
-?>
-<!DOCTYPE HTML>
+?><!DOCTYPE HTML>
 <html lang="fr-FR">
 	<head>
 		<meta charset="UTF-8" />
@@ -15,16 +9,16 @@ include dirname(__FILE__).'/inc/header.php';
 	</head>
 	<body>
 		<div id="main-container">
-		<h1>HTMLLisible</h1>
-		<form class="cssn_form" action="" method="post">
-			<div>
-				<label for="html_to_clean">HTML à réindenter</label>
-				<textarea rows="30" cols="100" style="display:block;" name="html_to_clean" id="html_to_clean"><?php echo htmlentities($content, ENT_COMPAT,'utf-8'); ?></textarea>
-			</div>
-			<div class="submit">
-				<button class="le-button"><span>Réindenter</span></button>				
-			</div>
-		</form>
+			<h1>HTMLLisible</h1>
+			<form class="cssn_form" action="" method="post">
+				<div>
+					<label for="html_to_clean">HTML à réindenter</label>
+					<textarea rows="30" cols="100" style="display:block;" name="html_to_clean" id="html_to_clean"><?php echo htmlentities($content, ENT_COMPAT,'utf-8'); ?></textarea>
+				</div>
+				<div class="submit">
+					<button class="le-button"><span>Réindenter</span></button>
+				</div>
+			</form>
 		</div>
 	</body>
 </html>
