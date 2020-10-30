ef53f5d996a0fc7dfccb6e2ac05d62cee69e4fc9
Kevin <kevin@rocher-dev.com>
2020-10-30 21:40:55 +0100

    Fix for PHP 7.4+

--- inc/header.php
+++ inc/header.php
@@ -1,13 +1,13 @@
 <?php
-include dirname( __FILE__ ).'/functions.php';
+include dirname(__FILE__) . '/functions.php';
 
 // Si les magic quotes sont activées
-if ( get_magic_quotes_gpc() ) {
-    function magicQuotes_awStripslashes( &$value, $key ) {
-        $value = stripslashes( $value );
+if (version_compare(phpversion(), '7.4', '<') && get_magic_quotes_gpc()) {
+    function magicQuotes_awStripslashes(&$value, $key) {
+        $value = stripslashes($value);
     }
-    $gpc = array( &$_GET, &$_POST, &$_COOKIE, &$_REQUEST );
-    array_walk_recursive( $gpc, 'magicQuotes_awStripslashes' );
+    $gpc = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+    array_walk_recursive($gpc, 'magicQuotes_awStripslashes');
 }
 
 $HTML_Lisible = new HTMLLisible();
